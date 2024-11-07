788b232a2ced504c54e88be7accc2cce0e8da6c9
MWessam <mohwessam@std.mans.edu.eg>
2024-11-07 20:16:36 +0200

    Episode 6 Vertex Attributes

---
+++ GameEngineSeries/Program.cs
@@ -0,0 +1,169 @@
+﻿using Silk.NET.Input;
+using Silk.NET.OpenGL;
+using Silk.NET.Windowing;
+using System;
+using Silk.NET.Maths;
+
+namespace Tutorial
+{
+    class Program
+    {
+        private static IWindow window;
+        private static GL Gl;
+
+        private static uint Vbo;
+        private static uint Ebo;
+        private static uint Vao;
+        private static uint Shader;
+
+        private static readonly string VertexShaderSource = @"
+        #version 330 core //Using version GLSL version 3.3
+        layout (location = 0) in vec2 vPos;
+        layout (location = 1) in vec4 vCol;
+        out vec4 vOutCol;
+        
+        void main()
+        {
+            gl_Position = vec4(vPos.x, vPos.y, 0.0f, 1.0);
+            vOutCol = vCol;
+        }
+        ";
+
+        private static readonly string FragmentShaderSource = @"
+        #version 330 core
+        in vec4 vOutCol;
+        out vec4 FragColor;
+
+        void main()
+        {
+            FragColor = vOutCol;
+        }
+        ";
+
+        private static readonly float[] Vertices =
+        {
+            // Pos          // Color
+             -0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 1.0f,
+             0.0f, 0.5f,   0.0f, 0.2f, 0.9f, 1.0f,
+             0.5f, -0.5f,  0.3f, 0.5f, 0.2f, 1.0f
+        };
+
+        private static void Main(string[] args)
+        {
+            var options = WindowOptions.Default;
+            options.Size = new Vector2D<int>(800, 600);
+            options.Title = "LearnOpenGL with Silk.NET";
+            window = Window.Create(options);
+
+            window.Load += OnLoad;
+            window.Render += OnRender;
+            window.Update += OnUpdate;
+            window.FramebufferResize += OnFramebufferResize;
+            window.Closing += OnClose;
+
+            window.Run();
+
+            window.Dispose();
+        }
+
+
+        private static unsafe void OnLoad()
+        {
+            IInputContext input = window.CreateInput();
+            for (int i = 0; i < input.Keyboards.Count; i++)
+            {
+                input.Keyboards[i].KeyDown += KeyDown;
+            }
+
+            Gl = GL.GetApi(window);
+            Vao = Gl.GenVertexArray();
+            Gl.BindVertexArray(Vao);
+            Vbo = Gl.GenBuffer();
+            Gl.BindBuffer(BufferTargetARB.ArrayBuffer, Vbo);
+            fixed (void* v = &Vertices[0])
+            {
+                Gl.BufferData(BufferTargetARB.ArrayBuffer, (nuint) (Vertices.Length * sizeof(uint)), v, BufferUsageARB.StaticDraw); //Setting buffer data.
+            }
+
+            uint vertexShader = Gl.CreateShader(ShaderType.VertexShader);
+            Gl.ShaderSource(vertexShader, VertexShaderSource);
+            Gl.CompileShader(vertexShader);
+
+            string infoLog = Gl.GetShaderInfoLog(vertexShader);
+            if (!string.IsNullOrWhiteSpace(infoLog))
+            {
+                Console.WriteLine($"Error compiling vertex shader {infoLog}");
+            }
+
+            uint fragmentShader = Gl.CreateShader(ShaderType.FragmentShader);
+            Gl.ShaderSource(fragmentShader, FragmentShaderSource);
+            Gl.CompileShader(fragmentShader);
+
+            infoLog = Gl.GetShaderInfoLog(fragmentShader);
+            if (!string.IsNullOrWhiteSpace(infoLog))
+            {
+                Console.WriteLine($"Error compiling fragment shader {infoLog}");
+            }
+
+            Shader = Gl.CreateProgram();
+            Gl.AttachShader(Shader, vertexShader);
+            Gl.AttachShader(Shader, fragmentShader);
+            Gl.LinkProgram(Shader);
+
+            Gl.GetProgram(Shader, GLEnum.LinkStatus, out var status);
+            if (status == 0)
+            {
+                Console.WriteLine($"Error linking shader {Gl.GetProgramInfoLog(Shader)}");
+            }
+
+            Gl.DetachShader(Shader, vertexShader);
+            Gl.DetachShader(Shader, fragmentShader);
+            Gl.DeleteShader(vertexShader);
+            Gl.DeleteShader(fragmentShader);
+            // Pos:
+            Gl.VertexAttribPointer(0, 2, VertexAttribPointerType.Float, false, 6 * sizeof(float), null);
+            Gl.EnableVertexAttribArray(0);
+            // Col:
+            Gl.VertexAttribPointer(1, 4, VertexAttribPointerType.Float, false, 6 * sizeof(float), 2 * sizeof(float));
+            Gl.EnableVertexAttribArray(1);
+        }
+
+        private static unsafe void OnRender(double obj) //Method needs to be unsafe due to draw elements.
+        {
+            Gl.Clear((uint) ClearBufferMask.ColorBufferBit);
+
+            Gl.BindVertexArray(Vao);
+            Gl.UseProgram(Shader);
+
+            //Draw the geometry.
+            Gl.DrawArrays(PrimitiveType.Triangles, 0, (uint)Vertices.Length);
+        }
+
+        private static void OnUpdate(double obj)
+        {
+
+        }
+
+        private static void OnFramebufferResize(Vector2D<int> newSize)
+        {
+            Gl.Viewport(newSize);
+        }
+
+        private static void OnClose()
+        {
+            //Remember to delete the buffers.
+            Gl.DeleteBuffer(Vbo);
+            Gl.DeleteBuffer(Ebo);
+            Gl.DeleteVertexArray(Vao);
+            Gl.DeleteProgram(Shader);
+        }
+
+        private static void KeyDown(IKeyboard arg1, Key arg2, int arg3)
+        {
+            if (arg2 == Key.Escape)
+            {
+                window.Close();
+            }
+        }
+    }
+}
